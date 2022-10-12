4cbe0036f3c31eb1f8c4bdce8803b7c0ef861d5c
Helashow <lucas.bnlsp2013@gmail.com>
2022-10-12 20:23:13 -0300

    create: tests for transaction view

--- transactions/tests.py
+++ transactions/tests.py
@@ -1,3 +1,5 @@
+from urllib import response
+
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework.authtoken.models import Token
@@ -37,6 +39,61 @@ class TransactionModelTest(TestCase):
         )
 
 
+class TransactionViewTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.transaction = Transaction.objects.create(
+            type= "Financiamento",
+            date= "2019-03-01",
+            value= "142.00",
+            cpf= "09620676017",
+            card= "4753****3153",
+            time= "15:34:53",
+            shop_owner= "JOÃO MACEDO",
+            shop= "BAR DO JOÃO"
+        )
 
+        user = User.objects.create_user(
+            username="user",
+            password="1234"
+        )
+
+        cls.token = Token.objects.create(user=user)
+
+        cls.retrieveUrl = reverse("retrieve", kwargs={"transaction_id": cls.transaction.id})
+        
+        cls.listUrl = reverse("list")
+
+    def test_list_transactions(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
+
+        response = self.client.get(self.listUrl)
+
+        expected_status_code = status.HTTP_200_OK
+
+        self.assertEqual(expected_status_code, response.status_code)
+
+    def test_unauthorized_list_transactions(self):
+        response = self.client.get(self.listUrl)
+
+        expected_status_code = status.HTTP_401_UNAUTHORIZED
+
+        self.assertEqual(expected_status_code, response.status_code)
+
+    def test_retrieve_transaction(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
+
+        response = self.client.get(self.retrieveUrl)
+
+        expected_status_code = status.HTTP_200_OK
+
+        self.assertEqual(expected_status_code, response.status_code)
+
+    def test_unauthorized_retrieve_transaction(self):
+        response = self.client.get(self.retrieveUrl)
+
+        expected_status_code = status.HTTP_401_UNAUTHORIZED
+
+        self.assertEqual(expected_status_code, response.status_code)
 
 
--- transactions/urls.py
+++ transactions/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from transactions import views
 
 urlpatterns = [
-    path("transactions/", views.TransactionView.as_view(), name = "list-create-transaction"),
-    path("transactions/<str:transaction_id>/", views.TransactionRetrieveView.as_view()),
+    path("transactions/", views.TransactionView.as_view(), name = "list"),
+    path("transactions/<str:transaction_id>/", views.TransactionRetrieveView.as_view(), name="retrieve"),
 ]
