53e13b17c6fca3cf44160863a6e114921791d16a
Helashow <lucas.bnlsp2013@gmail.com>
2022-10-12 15:40:12 -0300

    create: tests for transaction model

--- bank_transactions/settings.py
+++ bank_transactions/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+import dotenv
+
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-qi5(f(ftib$fp7n=ye4kia7!2w2=76-@)z$g979szua^jqn_u("
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
@@ -87,8 +92,12 @@ WSGI_APPLICATION = "bank_transactions.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": "127.0.0.1",
+        "PORT": 5432,
     }
 }
 
--- transactions/tests.py
+++ transactions/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+from rest_framework.views import status
+from users.models import User
 
-# Create your tests here.
+from transactions.models import Transaction
+
+
+class TransactionModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        pass
+
+    def test_transaction_creation(self):
+        transaction = Transaction.objects.create(
+            type= "Financiamento",
+            date= "2019-03-01",
+            value= "142.00",
+            cpf= "09620676017",
+            card= "4753****3153",
+            time= "15:34:53",
+            shop_owner= "JOÃO MACEDO",
+            shop= "BAR DO JOÃO"
+        )
+
+        self.assertIsNotNone(transaction.id)
+
+    def test_transaction_wrong_fields_creation(self):
+        with self.assertRaises(TypeError):
+            Transaction.objects.create(
+            type= 999,
+            date= True,
+            value= "142.00",
+            cpf= "09620676017",
+            card= "4753****3153",
+            time= "15:34:53",
+            shop_owner= "JOÃO MACEDO",
+            shop= "BAR DO JOÃO"
+        )
+
+
+
+
+
